89d945e24787bbea3b6f6d519bebe221047ea541
Fajardo peralta Leon <leon.fajardoperalta@elev.ga.ntig.se>
2025-01-29 14:58:22 +0100

    spel grej 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,43 @@
 ﻿Console.WriteLine("Your wake.");
-Console.ReadLine();
 
 float energi = 0;
 float energimax = 5;
+float guldmynt = 0;
+float guldmax = 20;
+float jobb = 1;
+// grejer<int>[guldmynt,]
 
-for (float i = 0; i < energimax; )
+static bool jaslashnej()
 {
+    bool kolla = false;
+    int suh = 0;
+    while (kolla == false && suh < 1 || suh > 2)
+    {
+        string svar = Console.ReadLine();
+        kolla = int.TryParse(svar, out suh);
 
+        if (suh == 1)
+        {
+            return true;
+        }
+        if (suh == 2)
+        {
+            return false;
+        }
+        Console.ReadLine();
+
+    }
+    return false;
+}
+
+bool gordet = jaslashnej();
+
+while (gordet == true && energi <= jobb)
+{
+    guldmynt = guldmynt + 2;
+    if (guldmynt <= guldmax)
+    {
+        guldmynt == guldmax; 
+    }
+    Console.WriteLine(guldmynt);
 }
